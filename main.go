d4a6740eb82e511a84ccab79014206f6e6d0517a
tony <tonyaleynikov@gmail.com>
2026-05-25 17:16:55 +0300

    setWebhook 2

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -13,8 +11,6 @@ import (
 
 func main() {
 	http.HandleFunc("GET /handler", handler)
-	http.HandleFunc("GET /getme", getMeHandler)
-	http.HandleFunc("GET /setWebhook", getWebhook)
 
 	s := &http.Server{
 		Addr: ":8080",
@@ -25,16 +21,6 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println("Err")
-		return
-	}
-	defer r.Body.Close()
-	fmt.Println(string(body))
-}
-
-func getMeHandler(w http.ResponseWriter, r *http.Request) {
 	token := "7121104577:AAG9TuCKKVJvCRVp7EzQ8rl-wBIgNwFLUzk"
 	ip := "149.154.167.220"
 
@@ -61,43 +47,5 @@ func getMeHandler(w http.ResponseWriter, r *http.Request) {
 
 	b, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(b))
-}
-
-func getWebhook(w http.ResponseWriter, r *http.Request) {
-	token := "7121104577:AAG9TuCKKVJvCRVp7EzQ8rl-wBIgNwFLUzk"
-	ip := "149.154.167.220"
-	webhookURL := "https://umore.serveousercontent.com"
-
-	c := &http.Client{
-		Timeout: 10 * time.Second,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				ServerName: "api.telegram.org",
-			},
-		},
-	}
-
-	payload := map[string]string{
-		"url": webhookURL,
-	}
-	bodyJSON, err := json.Marshal(payload)
-	if err != nil {
-		panic(err)
-	}
-
-	req, err := http.NewRequest(http.MethodPost, "https://"+ip+"/bot"+token+"/setWebhook", bytes.NewReader(bodyJSON))
-	if err != nil {
-		panic(err)
-	}
-	req.Host = "api.telegram.org"
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	b, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(b))
+	w.WriteHeader(resp.StatusCode)
 }
