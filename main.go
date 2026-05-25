3b4afb644798bcea3c63ad11e497bad3087556eb
tony <tonyaleynikov@gmail.com>
2026-05-25 15:27:53 +0300

    handler

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /handler", handler)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Listening on port 8080")
+	log.Fatal(s.ListenAndServe()) // почему именно log.Fatal
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Err")
+		return
+	}
+	defer r.Body.Close()
+	fmt.Println(string(body))
+}
